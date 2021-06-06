a5e1e2a6df51d8c1efbb950f638fb164cba8298f
Büşra Oğuzoğlu <busra.oguzoglu@gmail.com>
2021-06-06 16:39:44 +0300

    Add files via upload

---
+++ main_cnn.py
@@ -0,0 +1,153 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+from torchvision.utils import save_image
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Ref: https://github.com/lyeoni/pytorch-mnist-VAE/blob/master/pytorch-mnist-VAE.ipynb
+# Ref: https://towardsdatascience.com/building-a-convolutional-vae-in-pytorch-a0f54c947f71
+
+class VAE(nn.Module):
+    def __init__(self, imgChannels=1, featureDim=32*20*20, zDim=256):
+        super(VAE, self).__init__()
+
+        # Initializing the 2 convolutional layers and 2 full-connected layers for the encoder
+        self.encConv1 = nn.Conv2d(imgChannels, 16, 5)
+        self.encConv2 = nn.Conv2d(16, 32, 5)
+        self.encFC1 = nn.Linear(featureDim, zDim)
+        self.encFC2 = nn.Linear(featureDim, zDim)
+
+        # Initializing the fully-connected layer and 2 convolutional layers for decoder
+        self.decFC1 = nn.Linear(zDim, featureDim)
+        self.decConv1 = nn.ConvTranspose2d(32, 16, 5)
+        self.decConv2 = nn.ConvTranspose2d(16, imgChannels, 5)
+
+
+    def encoder(self, x):
+        # Input is fed into 2 convolutional layers sequentially
+        # The output feature map are fed into 2 fully-connected layers to predict mean (mu) and variance (logVar)
+        # Mu and logVar are used for generating middle representation z and KL divergence loss
+        x = F.relu(self.encConv1(x))
+        x = F.relu(self.encConv2(x))
+        x = x.view(-1, 32 * 20 * 20)
+        mu = self.encFC1(x)
+        logVar = self.encFC2(x)
+        return mu, logVar
+
+    def sampling(self, mu, log_var):
+        # Reparameterization takes in the input mu and logVar and sample the mu + std * eps
+        std = torch.exp(log_var / 2)
+        eps = torch.randn_like(std)
+        return mu + std * eps
+
+    def decoder(self, z):
+        # z is fed back into a fully-connected layers and then into two transpose convolutional layers
+        # The generated output is the same size of the original input
+        x = F.relu(self.decFC1(z))
+        x = x.view(-1, 32, 20, 20)
+        x = F.relu(self.decConv1(x))
+        x = torch.sigmoid(self.decConv2(x))
+        return x
+
+    def forward(self, x):
+        # The entire pipeline of the VAE: encoder -> reparameterization -> decoder
+        # output, mu, and logVar are returned for loss computation
+        mu, log_var = self.encoder(x)
+        z = self.sampling(mu, log_var)
+        out = self.decoder(z)
+        return out, mu, log_var
+
+def loss_function(recon_x, x, mu, log_var):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+    KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+    return BCE + KLD
+
+
+def train(epoch, vae, train_loader, optimizer):
+    vae.train()
+    train_loss = 0
+    for batch_idx, (data, _) in enumerate(train_loader):
+        data = data.cuda()
+        optimizer.zero_grad()
+
+        recon_batch, mu, log_var = vae(data)
+        loss = loss_function(recon_batch, data, mu, log_var)
+
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item() / len(data)))
+    print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, train_loss / len(train_loader.dataset)))
+
+
+def test(vae, test_loader):
+    vae.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for data, _ in test_loader:
+            data = data.cuda()
+            recon, mu, log_var = vae(data)
+
+            # sum up batch loss
+            test_loss += loss_function(recon, data, mu, log_var).item()
+
+    test_loss /= len(test_loader.dataset)
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+
+def main():
+
+    print('this is hw3')
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(device)
+
+    bs = 100
+    # MNIST Dataset
+    train_dataset = datasets.MNIST(root='./mnist_data/', train=True, transform=transforms.ToTensor(), download=True)
+    test_dataset = datasets.MNIST(root='./mnist_data/', train=False, transform=transforms.ToTensor(), download=False)
+
+    # Data Loader (Input Pipeline)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=bs, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=bs, shuffle=False)
+
+    vae = VAE()
+    if torch.cuda.is_available():
+        vae.cuda()
+
+    optimizer = optim.Adam(vae.parameters())
+
+    # Training:
+    for epoch in range(1, 15):
+        train(epoch, vae, train_loader, optimizer)
+        test(vae, test_loader)
+
+    with torch.no_grad():
+        z = torch.randn(64, 2).cuda()
+        sample = vae.decoder(z).cuda()
+        save_image(sample.view(64, 1, 28, 28), './samples/sample_' + '.png')
+
+    images, labels = iter(test_loader).next()
+    sample_pic = images[0]
+
+    #sample_pic = test_dataset[4]
+    plt.imshow(sample_pic[0].reshape(28, 28), cmap="gray")
+    plt.show()
+
+    with torch.no_grad():
+        sample_pic = sample_pic.cuda()
+        result = vae.forward(sample_pic)
+        print('Got result')
+        result = result[0].cpu()
+        plt.imshow(result.reshape(28, 28), cmap="gray")
+        plt.show()
+
+if __name__ == '__main__':
+    main()
+
